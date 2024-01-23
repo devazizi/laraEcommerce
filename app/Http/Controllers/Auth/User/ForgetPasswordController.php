0acb923f08e16b6c32b7756d1c766dd917b8cc3d
johndevs <dev.azizimail@gmail.com>
2024-01-23 20:07:56 +0330

    feature: add auth controller

---
+++ app/Http/Controllers/Auth/User/ForgetPasswordController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Auth\User;
+
+use App\Http\Controllers\Controller;
+
+class ForgetPasswordController extends Controller
+{
+    public function __invoke()
+    {
+
+    }
+}
---
+++ app/Http/Controllers/Auth/Vendor/RegisterController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Auth\Vendor;
+
+use App\Http\Controllers\Controller;
+
+class RegisterController extends Controller
+{
+    public function __invoke()
+    {
+
+    }
+}
