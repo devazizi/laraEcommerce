53dbca076fd59c330e68094c97ba78d1f6fca4f3
johndevs <dev.azizimail@gmail.com>
2024-01-23 19:45:45 +0330

    feature create vendor product table migration

--- database/migrations/2024_01_23_161207_create_vendor_product_table.php
+++ database/migrations/2024_01_23_161207_create_vendor_product_table.php
@@ -11,13 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('vendor_products', function (Blueprint $table) {
             $table->id();
-            $table->unsignedInteger('vendor_id');
-            $table->string('name_fa');
-            $table->string('name_en');
-            $table->unsignedInteger('product_category_id')->index();
-            // $table->string
             $table->timestamps();
         });
     }
@@ -27,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('vendor_products');
     }
 };
