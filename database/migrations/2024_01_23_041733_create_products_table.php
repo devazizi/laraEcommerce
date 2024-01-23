dc64a4e1c2e6d9c5844f8b07aabccabb92e3efcf
johndevs <dev.azizimail@gmail.com>
2024-01-23 07:49:34 +0330

    feature: create product category and product table migrations

---
+++ database/migrations/2024_01_23_041733_create_products_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('vendor_id');
+            $table->string('name_fa');
+            $table->string('name_en');
+            $table->unsignedInteger('product_category_id')->index();
+            $table->string
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
