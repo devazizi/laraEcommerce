85ca63445a0a5d8a06e333079e09db55ee385980
johndevs <dev.azizimail@gmail.com>
2024-01-23 20:15:22 +0330

    feature: add some controllers

---
+++ app/Http/Controllers/Auth/Vendor/ForgetPasswordController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Auth\Vendor;
+
+use App\Http\Controllers\Controller;
+
+class ForgetPasswordController extends Controller
+{
+    public function __invoke()
+    {
+
+    }
+}
